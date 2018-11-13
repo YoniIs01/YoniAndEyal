ee574f8077592ce5f5a3f9a18287705e6e191a17
YoniIs01 <yonatan.israeli@mail.huji.ac.il>
2018-11-13 18:08:15 +0200

    גודל קבוע של באונדינג בוקס בוורוני

--- Create_Rock_Image.m
+++ Create_Rock_Image.m
@@ -93,8 +93,10 @@ Rock_Matrix(Rock_Matrix==0)=100; %Grain boundary
 Rock_Image=label2rgb(Rock_Matrix);%assigning the labels to an image
 Rock_Matrix=double(Rock_Matrix);%turn Rock_Matrix from labels to double
 %% Find width and height of largest grain
-bb =regionprops(Rock_BW_Image,'BoundingBox');%find bounding boxes of grains
-bbMatrix = vertcat(bb(:).BoundingBox); %turning into matrix
-Height=max(bbMatrix(:,3));%largest height
-Width=max(bbMatrix(:,4));%largest width
+%  bb =regionprops(Rock_BW_Image,'BoundingBox');%find bounding boxes of grains
+%  bbMatrix = vertcat(bb(:).BoundingBox); %turning into matrix
+%  Height=max(bbMatrix(:,3));%largest height
+%  Width=max(bbMatrix(:,4));%largest width
+Height=129;
+Width=109;
 end
