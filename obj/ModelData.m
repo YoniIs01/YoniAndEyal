4b10a0f326504e8ca293db9036386ac507cb6c74
YoniIs01 <yonatan.israeli@mail.huji.ac.il>
2019-02-11 14:15:10 +0200

    הרצת סרטים מהר יותר

--- obj/ModelData.m
+++ obj/ModelData.m
@@ -31,6 +31,7 @@ classdef ModelData
             Current_Rock_Matrix = this.RockFirstImage;
             Current_Rock_Matrix (1,:) =0;
             Rock_Frames(1) = im2frame(label2rgb(Current_Rock_Matrix));
+            j = 2;
            for i=2:length(this.Steps) 
                DissolvedIndexes = setdiff(this.Steps(i-1).SolutionContactLinearIndex,this.Steps(i).SolutionContactLinearIndex);
                Current_Rock_Matrix(DissolvedIndexes) = 0;
@@ -40,13 +41,18 @@ classdef ModelData
                 %the pixels that should be dissolved during this time step (including areas
                 %outside bounding box)
                 Current_Rock_Matrix([Temp{:}])=0; %dissolving the chunks
-                Rock_Frames(i) = im2frame(label2rgb(Current_Rock_Matrix));
+                if (mod(i,16) == 0)
+                    Rock_Frames(j) = im2frame(label2rgb(Current_Rock_Matrix));
+                    j = j+1;
+                end
            end
            implay(Rock_Frames);
         end
         function PlaySurfaceMovie(this)
-            for i = 1:length(this.Steps)
-                Rock_Frames(i) = im2frame(label2rgb(this.GetSurfaceMatrixByStep(i)));
+            j = 1;
+            for i = 1:16:length(this.Steps)
+                Rock_Frames(j) = im2frame(label2rgb(this.GetSurfaceMatrixByStep(i)));
+                j = j + 1;
             end
             implay(Rock_Frames);
         end
